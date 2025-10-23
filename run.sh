3acb560a222790b07afa4e87c4fdb57a4cf1116e
John Stebbins <jstebbins.hb@gmail.com>
2025-10-23 08:13:08 -0600

    add option processing to run.sh
    Makes spoken language and model configurable at runtime

--- run.sh
+++ run.sh
@@ -1,11 +1,43 @@
 #!/bin/bash
 
-# run.sh [options]
-# -l <lang>
-# -m <model>
+# Usage: run.sh [ -l lang ] [ -m model ] [ -h ]
+# Options:
+#   -l lang     - Spoken language
+#   -m model    - Model to load
+#   -h          - Show this message
+
+usage()
+{
+    cat <<EOF
+Usage: $0 [ -l lang ] [ -m model ] [ -h]
+Options:
+    -l lang     - Spoken language
+    -m model    - Model to load
+    -h          - Show this message
+EOF
+}
 
 lang=en
 model=large-v3-turbo
+
+while getopts "l:m:h" opt; do
+    case ${opt} in
+        l)
+            lang=${OPTARG}
+            ;;
+        m)
+            model=${OPTARG}
+            ;;
+        h)
+            usage
+            exit 0
+            ;;
+        *)
+            usage
+            exit 1
+            ;;
+    esac
+done
 
 whisper-server -pr -pp -fa -l ${lang} -m /opt/whisper.cpp/models/ggml-${model}.bin --host 127.0.0.1 --port 8910 &
 /opt/wyoming-whisper-api-client/.venv/bin/wyoming-whisper-api-client --api http://127.0.0.1:8910/inference --uri tcp://0.0.0.0:10300 &
